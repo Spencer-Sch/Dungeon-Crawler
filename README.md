55398961b683d72ee6cc7b5c013412a7a65aacd1
Spencer-Sch <spencer.schoeneman@gmail.com>
2021-03-11 21:47:06 -0600

    See day 4 of README.

--- README.md
+++ README.md
@@ -24,3 +24,11 @@ Day 3
         * "gainWeapon" functionality started.
     * Added check so "Search" can only be executed once per room.
         * Modal window is now triggered if player attemtps to search in the same room more than once.
+
+Day 4
+* Adjusted percentages for 'good find' or 'bad find' selection.
+* Added weapon selection logic for when the player finds a new weapon inside of a 'good find' occurence.
+    * Added modal window alerts to inform the player of what they have found.
+    * Added logic to swap out the player's old weapon for the newly found weapon.
+        * Needs work.  found-weapon-alerts are not responding to weapon-exclusivity-logic.
+* Attempting to use a Set to keep track of all weapons the player has previously aquired.
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -15,21 +15,21 @@ const dungeonData = {
                 normalAtkModifier: 10,
                 bigAtkModifier: 15,
                 specialAtk: null, // make a function?
-                rarityModifier: 5,
+                // rarityModifier: 5,
             },
             axe: {
                 baseAtkDmg: 20,
                 normalAtkModifier: 20,
                 bigAtkModifier: 30,
                 specialAtk: null, // make a function?
-                rarityModifier: 10,
+                // rarityModifier: 10,
             },
             wand: {
                 baseAtkDmg: 0,
                 normalAtkModifier: 0,
                 bigAtkModifier: 0,
                 specialAtk: null, // spend all potions = insta kill
-                rarityModifier: 25,
+                // rarityModifier: 25,
             },
 
         },
@@ -42,7 +42,7 @@ const dungeonData = {
     playerData: {
         playerHealth: 100,
         playerPotionCount: 1,
-        playerCurrentWeapon: 'dagger',
+        playerCurrentWeapon: 'DAGGER',
         playerPrevWeapons: [],
     },
     monsterData: {
@@ -79,7 +79,6 @@ const WEAPON_AXE = dungeonData.items.weapons.axe;
 const WEAPON_WAND = dungeonData.items.weapons.wand;
 const ITEM_POTION = dungeonData.items.potions;
 const PLAYER_DATA = dungeonData.playerData;
-const PLAYER_CUR_WEAPON = dungeonData.playerData.playerCurrentWeapon;
 const PLAYER_PREV_WEAPONS = dungeonData.playerData.playerPrevWeapons;
 const MONSTER_DATA = dungeonData.monsterData;
 const MONSTER_LVL1 = dungeonData.monsterData.monsterLevels.level_1;
@@ -102,6 +101,8 @@ const MODAL_WINDOW = document.getElementById('item-modal');
 const MODAL_TEXT = document.getElementById('item-modal-text');
 const MODAL_BTN = document.getElementById('item-modal-btn');
 const MODAL_BACKDROP = document.getElementById('backdrop');
+const POTIONS_COUNTER = document.getElementById('potions-span');
+const CURRENT_WEAPON_UI = document.getElementById('weapon-span');
 
 /*===========================================
 Program Start
@@ -109,7 +110,14 @@ Program Start
 // Initialize page with randomly selected first image to reduce delay upong 'Start Game'
 DUNGEON_IMG.setAttribute('src', `/assets/images/corridor${Math.floor(Math.random() * 8 + 1)}.jpg`);
 
+// Initialize the set that will keep track of which weapons the player has had throughout the game.
+const prevWeaponSet = new Set();
+prevWeaponSet.add('DAGGER');
+PLAYER_DATA.playerPrevWeapons = prevWeaponSet;
 
+const prevImgNum = [];
+
+// This changes the UI when the 'Start Game' btn is clicked
 const startGameHandler = () => {
     START_BTN_CONTAINER.classList.add('invisible');
     H1.classList.add('invisible');
@@ -118,8 +126,7 @@ const startGameHandler = () => {
     NON_COMBAT_UI.classList.remove('invisible');
 };
 
-const prevImgNum = [];
-
+// This chooses and displays the new dungeon image each time the 'Move Forward' btn is clicked.
 const dungeonImageHandler = () => {
     ROOM_DATA.searchedCurrentRoom = false;
     let randCorridorNum = Math.floor(Math.random() * 8 + 1);
@@ -155,36 +162,75 @@ const closeModal = () => {
 
 const gainPotion = () => {
     PLAYER_DATA.playerPotionCount++;
+    POTIONS_COUNTER.textContent = PLAYER_DATA.playerPotionCount;
 };
 
-const gainWeapon = () => {
+const modalAlert = (message) => {
+    MODAL_TEXT.textContent = message;
+    openModal();
+    MODAL_BTN.removeEventListener;
+    MODAL_BTN.addEventListener('click', closeModal);
+};
+
+// HAVING PROBLEM: there is some sort of disconnect between what weapons the player has found/currnetly has and what appears in modal text alerts when searching the dungeon.
+// This swaps out the player's current weapon with the new one they just found.
+const equipNewWeapon = (weapon) => {
+    if (!prevWeaponSet.has(weapon)) {
+        modalAlert(`You found a(n) ${weapon}!`);
+        prevWeaponSet.add(PLAYER_DATA.playerCurrentWeapon);
+        PLAYER_DATA.playerCurrentWeapon = weapon;
+        CURRENT_WEAPON_UI.textContent = PLAYER_DATA.playerCurrentWeapon;
+    } else {
+        modalAlert(`Just another rusty ${weapon}...`);
+    }
+    console.log("your new weapon is:" + PLAYER_DATA.playerCurrentWeapon);
+    console.log(PLAYER_DATA.playerPrevWeapons);
+};
+
+// This determains which weapon the player found.
+const selectNewWeapon = () => {
+    let newWeapon = "";
     let randWeaponNum = Math.floor(Math.random() * 100 + 1);
-    // How to select weapon?
+    if (randWeaponNum <= 50) {
+        newWeapon = "DAGGER"
+    } else if (randWeaponNum > 50 && randWeaponNum <= 75) {
+        newWeapon = "SWORD";
+    } else if (randWeaponNum > 75 && randWeaponNum <= 92) {
+        newWeapon = "AXE";
+    }  else if (randWeaponNum > 92) {
+        newWeapon = "WAND";
+    }
+    equipNewWeapon(newWeapon);
 };
 
+// const badFindHandler = (num) => {
+
+// };
+
+// This determains whether the good thing the player found is a weapon or a potion.
 const goodFindHandler = (num) => {
-    if (num >= 70) {
+    if (num > 8) {
         gainPotion();
     } else {
-        gainWeapon();
+        selectNewWeapon();
     }
 };
 
+// Runs when the 'Search' btn is clicked
 const searchDungeonHandler = () => {
     if (!ROOM_DATA.searchedCurrentRoom) {
         ROOM_DATA.searchedCurrentRoom = true;
-        let randSearchNum1 = Math.floor(Math.random() * 100 + 1);
-        let randSearchNum2 = Math.floor(Math.random() * 100 + 1);
-        if (randSearchNum1 >= 80) {
+        let randSearchNum1 = Math.floor(Math.random() * 10 + 1);
+        let randSearchNum2 = Math.floor(Math.random() * 10 + 1);
+        console.log('Good vs Bad Find Number:' + randSearchNum1);
+        if (randSearchNum1 > 7) { // This determains if the player 'rolled' high enough to find something good or not.
             goodFindHandler(randSearchNum2);
         } else {
-            badFindHandler(randSearchNum2);
+            console.log("BAD FIND");
+            // badFindHandler(randSearchNum2);
         }
     } else {
-        MODAL_TEXT.textContent = 'Oops, you\'ve already searched this room!';
-        openModal();
-        MODAL_BTN.removeEventListener;
-        MODAL_BTN.addEventListener('click', closeModal);
+        modalAlert('Oops, you\'ve already searched this room!');
     }
 };
 
